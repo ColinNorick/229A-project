196157e84279773a253b3fa8686dcbd5ef5fea03
ColinNorick <colin.norick@gmail.com>
2019-06-02 01:33:26 -0700

    basic NN working

--- graphics.py
+++ graphics.py
@@ -9,7 +9,7 @@ def draw_game(game):
     counter = 0
     for i in np.nditer(game):
         counter +=1
-        if counter % 6 ==1:
+        if counter % 7 ==1:
             print("\n |", end ="")
         if i ==  -1:
             print (" O |", end ="")
@@ -17,3 +17,28 @@ def draw_game(game):
             print (" X |", end ="")
         if i ==  0:
             print (" _ |", end ="")
+
+def translate_game(game):
+    game = np.array([int(x) for x in game])
+    board = np.zeros((6,7))
+    position = {}
+    player = 0
+    for i in  np.nditer(game):
+        i = int(i) - 1
+
+        if i not in position.keys():
+            position[i] = 5
+
+        else:
+            position[i] = position[i] - 1
+        if player % 2 == 0:
+            
+            board[position[i]][i] = 1
+        else:
+
+            board[position[i]][i] = -1
+        player = player + 1
+    #print (board)
+    return board
+
+
--- testing_suite.py
+++ testing_suite.py
@@ -1,12 +1,27 @@
 # Everything is run from this file
 import numpy as np
-import matplotlib as plt
-import sklearn
-import graphics as gh
+from graphics import *
+import data_process as dp
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import MinMaxScaler
 
-a = np.ones((6,7))
-print (a)   
-gh.draw_game(a)
+
+#a = np.ones((6,7))
+#print (a)   
+#gh.draw_game(a)
+# test = "5554224333234511764415115"
+# game = translate_game(test)
+# draw_game(game)
+
 
 
 #processing the data
+X,y = dp.process()
+
+
+mlp = MLPClassifier(verbose=0, random_state=0)
+mlp.fit(X, y)
+print("Training set score: %f" % mlp.score(X, y))
+print("Training set loss: %f" % mlp.loss_)
+
