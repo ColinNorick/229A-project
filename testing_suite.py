36f854bf604c84d90c235e6983de5a28016c1966
ColinNorick <colin.norick@gmail.com>
2019-06-06 16:16:43 -0700

    Rough Framework

--- testing_suite.py
+++ testing_suite.py
@@ -20,8 +20,65 @@ from sklearn.preprocessing import MinMaxScaler
 X,y = dp.process()
 
 
-mlp = MLPClassifier(verbose=0, random_state=0)
-mlp.fit(X, y)
-print("Training set score: %f" % mlp.score(X, y))
-print("Training set loss: %f" % mlp.loss_)
+params = [{'solver': 'sgd', 'learning_rate': 'constant', 'momentum': 0,
+           'learning_rate_init': 0.4},
+          {'solver': 'sgd', 'learning_rate': 'constant', 'momentum': .0,
+          'learning_rate_init': .0001},
+          {'solver': 'sgd', 'learning_rate': 'constant', 'momentum': .0,
+          'learning_rate_init': .0003},
+          {'solver': 'sgd', 'learning_rate': 'constant', 'momentum': .0,
+          'learning_rate_init': .0001},
+          {'solver': 'sgd', 'learning_rate': 'constant', 'momentum': .0,
+          'learning_rate_init': .0001},
+          {'solver': 'sgd', 'learning_rate': 'constant', 'momentum': .0,
+          'learning_rate_init': .0001},]
+
+labels = ["high learning rate", "low learning rate ",
+          "constant with Nesterov's momentum",
+          "inv-scaling learning-rate", "inv-scaling with momentum",
+          "inv-scaling with Nesterov's momentum", "adam"]
+
+plot_args = [{'c': 'red', 'linestyle': '-'},
+             {'c': 'green', 'linestyle': '-'},
+             {'c': 'blue', 'linestyle': '-'},
+             {'c': 'red', 'linestyle': '--'},
+             {'c': 'green', 'linestyle': '--'},
+             {'c': 'blue', 'linestyle': '--'},
+             {'c': 'black', 'linestyle': '-'}]
+
+
+def plot_on_dataset(X, y, ax, name):
+    # for each dataset, plot learning for each learning strategy
+    print("\nlearning on dataset %s" % name)
+    ax.set_title(name)
+    #X = MinMaxScaler().fit_transform(X)
+    mlps = []
+    for label, param in zip(labels, params):
+        print("training: %s" % label)
+        mlp = MLPClassifier(verbose=0, random_state=1,
+                            max_iter=200, **param)
+        mlp.fit(X, y)
+        mlps.append(mlp)
+        print("Training set score: %f" % mlp.score(X, y))
+        print("Training set loss: %f" % mlp.loss_)
+    for mlp, label, args in zip(mlps, labels, plot_args):
+            ax.plot(mlp.loss_curve_, label=label, **args)
+
+#Where things start
+
+
+fig, axes = plt.subplots(2, 2, figsize=(15, 10))
+
+
+
+for ax, name in zip(axes.ravel(), ['High Learning Rate', 'Low Learning Rate',
+                                                    'momentum +', 'momentum -']):
+    plot_on_dataset(X, y, ax=ax, name=name)
+
+fig.legend(ax.get_lines(), labels, ncol=3, loc="upper center")
+plt.show()
+# mlp = MLPClassifier(verbose=0, random_state=0)
+# mlp.fit(X, y)   
+# print("Training set score: %f" % mlp.score(X, y))
+# print("Training set loss: %f" % mlp.loss_)
 
