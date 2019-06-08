65fd2f0fa15a257319298d086be946748107bfb9
Antonio Ferris <antonioferris01@gmail.com>
2019-06-08 14:29:26 -0700

    cleaned AI.py

--- AI.py
+++ AI.py
@@ -11,6 +11,16 @@ def undo_what_we_did_before(pos, board_rank):
 		return [-x for x in board_rank]
 	return board_rank
 
+def who_wins(board_rank):
+	max_score = np.max(board_rank)
+	if max_score > 0:
+		return 1
+	elif max_score < 0:
+		return -1
+	else:
+		return 0
+
+
 def find_all_moves(board_rank):
 	max_score = np.max(board_rank)
 	return [int(x == max_score) for x in board_rank]
@@ -23,7 +33,7 @@ def find_all_moves_idx(board_rank):
 			r.append(i)
 	return r
 
-def get_data(filename, use_multilabel=True):
+def get_data(filename, use_multilabel=False, use_weak=True):
 	X = []
 	y = []
 	with open(filename, 'r') as f:
@@ -34,41 +44,65 @@ def get_data(filename, use_multilabel=True):
 			board = bc.translate_game(line_data[0])
 			X.append(board.flatten())
 			board_ranks = undo_what_we_did_before(line_data[0], [ int(x) for x in line_data[1:] ])
-			if not use_multilabel:
-				# add the output to the data
-				max_rank = np.argmax(board_ranks)
-				y.append(max_rank)
-			else:
-				max_ranks = find_all_moves_idx(board_ranks)
-				y.append(max_ranks)
+			y.append(board_ranks)
 			# y.append([0 if i != max_rank else 1 for i in range(7)])
-	if use_multilabel:		
-		print('pre-multilabel stuff:')
-		print(y[:5])
-		mlb = MultiLabelBinarizer()
-		y = mlb.fit_transform(y)
-		print('post-multilabel stuff:')
-		print(y[:5])
 	return X, y
+
+def translate_data_to_multilabel(y):
+	'''
+	Translates y data of the form:
+	-3 4 -3 -3 -3 2 0 (scores)
+	into y data of the form:
+	1 where the data is 1 for a forced win,
+	0 for a draw, and -1 for a forced loss
+	'''
+	for ranks in y:
+		max_ranks = find_all_moves_idx(board_ranks)
+		y.append(max_ranks)
+	mlb = MultiLabelBinarizer()
+	y2 = mlb.fit_transform(y)
+
+def translate_data_to_whowins(y):
+	'''
+	Translates y data of the form:
+	-3 4 -3 -3 -3 2 0 (scores)
+	into y data of the form:
+	[0 1 0 0 0 1 0] where the data is 1 for
+	a move that acheives the best possible outcome,
+	and a 1 otherwise
+	'''
+	y2 = []
+	for ranks in y:
+		winner = who_wins(board_ranks)
+		y2.append(winner)
+
 
 def mlp_ai(X, y):
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 	print("Small subset of data")
 	print(X[:5])
 	print(y[:5])
-	param={'max_iter' : 250, 'hidden_layer_sizes': (100, 100)}
+	param={'max_iter' : 250, 'hidden_layer_sizes': (100, 50)}
 	mlp = MLPClassifier(**param)
 	mlp.fit(X_train, y_train)
 	print("Training set score: %f" % mlp.score(X_train, y_train))
 	print("Test set score: %f" % mlp.score(X_test, y_test))
+	
 	return mlp
 
 
 def main():
 	X,y = get_data('bulk_one-ALL.csv')
-	print('starting mlp!')
-	mlp = mlp_ai(X, y)
-	with open('mlp-trained-bulk.mlp', 'wb') as f:
+	y_whowins = translate_data_to_whowins(y)
+	y_multilabel = translate_data_to_multilabel(y)
+	print('starting mlp whowins!')
+	mlp = mlp_ai(X, y_whowins)
+	with open('mlp-trained-bulk-whowins.mlp', 'wb') as f:
+		pickle.dump(mlp, f)
+
+	print('starting mlp multilabel!')
+	mlp = mlp_ai(X, y_whowins)
+	with open('mlp-trained-bulk-multilabel.mlp', 'wb') as f:
 		pickle.dump(mlp, f)
 
 
